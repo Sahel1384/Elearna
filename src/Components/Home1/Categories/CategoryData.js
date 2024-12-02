a977d15f88830c4afe7ceffe8d15835300fbae12
Sahel1384 <pbsyousufzoydreaminghigh@gmail.com>
2024-12-02 10:28:35 +0430

    Elearna React Tailwind

---
+++ src/Components/Home1/Categories/CategoryData.js
@@ -0,0 +1,166 @@
+import card1 from "../../../assets/images/Home1/6710c8cf232065a7046261dc_Course Image-7-p-800.webp";
+import card2 from "../../../assets/images/Home1/6710bcf4976ba1d072d8b476_Course Image-4-2-p-800.webp";
+import card3 from "../../../assets/images/Home1/66eb9e25c4d132b7abc4dcb2_Course Small Image-8-p-500.webp";
+import card4 from "../../../assets/images/Home1/66eb9e1184c0c12d2244968d_Course Small Image-9-p-800.webp";
+import card5 from "../../../assets/images/Home1/66eb9e363f6e47ec5245cf09_Course Small Image-7-p-800.webp";
+import card6 from "../../../assets/images/Home1/6710bd0293bbfbd5e9b9c0b4_Course Image-5-p-500.webp";
+import card7 from "../../../assets/images/Home1/66fe6841120ac0d82bf8c08a_Digital Marketing.webp";
+import card8 from "../../../assets/images/Home1/course.webp";
+import card9 from "../../../assets/images/Home1/66eb9dfb3e87f8984b5a15b6_Course Small Image-p-800.webp";
+import card10 from "../../../assets/images/Home1/66ff6a72dd07492f0cbb0a6d_Blog Small Thumbnail-10-p-500.webp";
+import card11 from "../../../assets/images/Home1/6710bcf4976ba1d072d8b476_Course Image-4-2-p-800.webp";
+export const CategoryData1 = [
+  {
+    id: 1,
+    title: "MANAGEMENT",
+    desc: "Crisis management and problem-solving strategies",
+    btn1: "6 Lessons",
+    btn2: "8k Enrolled",
+    img: card1,
+    badge: "Free",
+  },
+  {
+    id: 2,
+    title: "DESIGN",
+    desc: "Advanced photoshop techniques for designers",
+    btn1: "8 Lessons",
+    btn2: "25k Enrolled",
+    img: card2,
+    badge: "Free",
+  },
+  {
+    id: 3,
+    title: "FINANCE",
+    desc: "Operations management and process improvement",
+    btn1: "12 Lessons",
+    btn2: "23k Enrolled",
+    img: card3,
+    badge: "Free",
+  },
+  {
+    id: 4,
+    title: "DEVELOPMENT",
+    desc: "Database integration with MongoDB",
+    btn1: "10 Lessons",
+    btn2: "7k Enrolled",
+    img: card4,
+    badge: "59$",
+  },
+  {
+    id: 5,
+    title: "DEVELOPMENT",
+    desc: "Database integration with MongoDB",
+    btn1: "10 Lessons",
+    btn2: "7k Enrolled",
+    img: card5,
+    badge: "49$",
+  },
+  {
+    id: 6,
+    title: "DEVELOPMENT",
+    desc: "Database integration with MongoDB",
+    btn1: "10 Lessons",
+    btn2: "7k Enrolled",
+    img: card6,
+    badge: "72$",
+  },
+];
+export const CategoryData2 = [
+  {
+    id: 1,
+    title: "DESIGN",
+    desc: "Design systems and pattern libraries",
+    btn1: "8 Lessons",
+    btn2: "25k Enrolled",
+    img: card7,
+    badge: "99$",
+  },
+  {
+    id: 2,
+    title: "DESIGN",
+    desc: "Advanced photoshop techniques for designers",
+    btn1: "8 Lessons",
+    btn2: "25k Enrolled",
+    img: card8,
+    badge: "79$",
+  },
+  {
+    id: 3,
+    title: "DESIGN",
+    desc: "Learn interactive Web Design by building real projects",
+    btn1: "8 Lessons",
+    btn2: "25k Enrolled",
+    img: card2,
+    badge: "Free",
+  },
+];
+
+export const CategoryData3 = [
+  {
+    id: 1,
+    title: "DEVELOPMENT",
+    desc: "Building single page applications with angular",
+    btn1: "8 Lessons",
+    btn2: "25k Enrolled",
+    img: card9,
+    badge: "Free",
+  },
+  {
+    id: 2,
+    title: "DEVELOPMENT",
+    desc: "Database integration with MongoDB",
+    btn1: "8 Lessons",
+    btn2: "25k Enrolled",
+    img: card4,
+    badge: "59$",
+  },
+  {
+    id: 3,
+    title: "DEVELOPMENT",
+    desc: "Full-stack development with the MERN stack",
+    btn1: "8 Lessons",
+    btn2: "25k Enrolled",
+    img: card10,
+    badge: "72$",
+  },
+];
+export const CategoryData4 = [
+  {
+    id: 1,
+    title: "MANAGEMENT",
+    desc: "Risk management and business continuity",
+    btn1: "8 Lessons",
+    btn2: "25k Enrolled",
+    img: card5,
+    badge: "49$",
+  },
+  {
+    id: 2,
+    title: "MANAGEMENT",
+    desc: "Grow your brand and reach your audience",
+    btn1: "8 Lessons",
+    btn2: "25k Enrolled",
+    img: card11,
+    badge: "39$",
+  },
+];
+export const CategoryData5 = [
+  {
+    id: 1,
+    title: "FINANCE",
+    desc: "Operations management and process improvement",
+    btn1: "8 Lessons",
+    btn2: "25k Enrolled",
+    img: card3,
+    badge: "Free",
+  },
+  {
+    id: 2,
+    title: "FINANCE",
+    desc: "The complete guide to personal and business finance",
+    btn1: "8 Lessons",
+    btn2: "25k Enrolled",
+    img: card11,
+    badge: "99$",
+  },
+];
